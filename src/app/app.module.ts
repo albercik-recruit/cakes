b324cc48bcd8b36debcc45fa9b44b64edcaa134c
Albert Leśnikowski <albert.lesnikowski@gmail.com>
2017-12-12 00:06:30 +0000

    Basic components + basic routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { CakeListComponent } from './cake-list/cake-list.component';
+import { CakeDetailsComponent } from './cake-details/cake-details.component';
+import {RouterModule, Routes} from '@angular/router';
 
+const appRoutes: Routes = [
+  {path: 'cakes-list', component: CakeListComponent},
+  {path: 'cake-details', component: CakeDetailsComponent},
+  {path: '**', component: CakeListComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CakeListComponent,
+    CakeDetailsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes,{enableTracing: false})
   ],
   providers: [],
   bootstrap: [AppComponent]
